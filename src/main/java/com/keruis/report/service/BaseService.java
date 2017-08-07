1d9072b3549fce93c686fa8892614cd404d0182f
sy <769797226@qq.com>
2017-08-07 15:30:49 +0800

    sy

---
+++ src/main/java/com/keruis/report/service/BaseService.java
@@ -0,0 +1,11 @@
+package com.keruis.report.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/8/7.
+ */
+@Service
+public class BaseService {
+    
+}
